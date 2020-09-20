cc6364f8b293dc73dfebe3ca935a812355817a2e
Ezequiel Regaldo <regaldo.ezequiel@gmail.com>
2020-09-20 00:10:55 -0300

    Chichito inc

---
+++ src/Aplicaciones/Municipio chichito/script.js
@@ -0,0 +1,40 @@
+let edadInput = document.getElementById('edad');
+let btnVerificar = document.getElementById('verificar');
+let formManejo = document.getElementById('formManejo');
+let requisitosAdicionales = document.getElementById('requisitosAdicionales')
+
+function verificarEdadParaConducir(){
+    let edad = edadInput.value
+
+    
+
+    if (edad < 18) {
+        alert('No tienes la edad suficiente para hacer el trámite')
+    }
+    else if (edad >= 18 && edad < 65) {
+        alert('Tienes los requisitos necesarios para conducir')
+    }
+    else if (edad >= 65) {
+        alert('Sos demasiado viejo')
+    }
+}
+
+formManejo['discapacidad'][0].addEventListener('click', e => {
+    requisitosAdicionales.style.display = "block";
+})
+formManejo['discapacidad'][1].addEventListener('click', e => {
+    requisitosAdicionales.style.display = "none";
+})
+
+console.log(formManejo['discapacidad'])
+
+
+formManejo.addEventListener('submit', event => {
+
+    event.preventDefault();
+    
+    verificarEdadParaConducir();
+
+})
+
+
